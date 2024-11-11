a148f884b493e8c28ae859a2ab3dcaca081c9eff
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-11-11 20:23:54 +0200

    Dealing with dictionaries

--- Data Structures/Dictionaries.py
+++ Data Structures/Dictionaries.py
@@ -1,6 +1,19 @@
-my_dict = {
+person = {
     "name": "Paulin",
     "age": 17,
-    "city": "Kigali"
+    "profession": "AI Engineere"
 }
-print(my_dict["name"])
+print(person["name"])
+print(person.get("age"))#Using get to access a dictionary
+person["city"] = "Kigali"# Adding a new key value pair
+print(person)
+person["age"] = 18#updating the existing data
+print(person["age"])
+removed_profession = person.pop("profession")
+print(removed_profession)#removing an item and returning its value
+
+del person["city"]
+print(person) #deletes an item
+
+last_item = person.popitem()
+
