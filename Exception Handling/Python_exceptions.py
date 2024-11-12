e336224c94aa24a76f565cf32a8cba82bf1c46be
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-11-12 07:09:17 +0200

    Started python_exceptions

---
+++ Exception Handling/Python_exceptions.py
@@ -0,0 +1,5 @@
+# What is an Exception?
+# An exception is an event that disrupts the normal flow of a program’s execution.
+# For example, trying to divide by zero, accessing a file that doesn’t exist,
+# or using a variable that hasn’t been defined yet will raise an exception.
+
