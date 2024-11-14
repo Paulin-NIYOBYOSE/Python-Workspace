c094d8f8c1e38aedba241d1465a5f40d5ebe812c
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-11-14 19:51:40 +0200

    Dealing with exception handling

--- Exception Handling/Python_exceptions.py
+++ Exception Handling/Python_exceptions.py
@@ -1,5 +1,11 @@
 # What is an Exception?
 # An exception is an event that disrupts the normal flow of a program’s execution.
-# For example, trying to divide by zero, accessing a file that doesn’t exist,
-# or using a variable that hasn’t been defined yet will raise an exception.
+# For example, trying to divide by zero, accessing a file that doesn’t exist,..
 
+
+try:
+    # Code that might raise an exception
+    result = 10 / 0
+except ZeroDivisionError:
+    # Code that runs if a ZeroDivisionError occurs
+    print("You can't divide by zero!")
