7b0f55d27d7b38982ae3a5395d8c499787262717
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-11-19 11:03:21 +0200

    Learning recude,filter and map

---
+++ Functional Programming/filter.py
@@ -0,0 +1,5 @@
+#filter: filters elements based on a specific condition and returns an iterable of elements that satisfy the condition
+#filtering odd numbers using filter
+numbers = [1,2,3,4,5,6,7,8]
+odd_numbers = filter(lambda x: x % 2 != 0, numbers)
+print(list(odd_numbers))
