b9e956dd158ea102a25fcce5f9202b0d3d95fe13
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-11-19 10:38:20 +0200

    Learning lambda functions

--- Functional Programming/Lambda.py
+++ Functional Programming/Lambda.py
@@ -6,3 +6,12 @@
 numbers = [1,2,3,4,5]
 squared = list(map(lambda x: x**2, numbers))
 print(squared)
+
+#using lambda as sorting keys
+data = [{'name': 'Paulin', 'age': 18}, {'name':'shem', 'age': 26},{'name': 'Niyobyose', 'age': 19}]
+sorted_data = sorted(data, key=lambda k: k["age"])
+print(sorted_data)
+
+#using lambda in callbacks
+button.on_click(lambda: print("Button clicked!"))
+
